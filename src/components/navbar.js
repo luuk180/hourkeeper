c8cefb5723286ce84d9d33f1160e327d3bf4c003
Luuk Volkering <luuk.volkering@gmail.com>
2021-03-06 12:22:45 +0100

    Made the navbar a little bit leaner

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,4 +1,4 @@
-import { FormControl, Nav, Navbar, Form, Button } from "react-bootstrap";
+import { Nav, Navbar, Form, Button } from "react-bootstrap";
 
 function DefaultNavigation(props) {
     if(props.loggedIn === false) {
@@ -8,12 +8,9 @@ function DefaultNavigation(props) {
                 <Navbar.Toggle aria-controls="default-navbar" />
                 <Navbar.Collapse id="default-navbar">
                     <Nav className="mr-auto">
-                        <Nav.Link href="#home">{props.home}</Nav.Link>
-                        <Nav.Link href="#login">{props.about}</Nav.Link>
+                        <Nav.Link href="#home">Home</Nav.Link>
                     </Nav>
                     <Form inline>
-                        <FormControl type="text" isRequired placeholder="Username"
-                            className="mr-sm-2" />
                         <Button variant="outline-success">Login</Button>
                     </Form>
                 </Navbar.Collapse>
@@ -26,14 +23,8 @@ function DefaultNavigation(props) {
                 <Navbar.Toggle aria-controls="default-navbar" />
                 <Navbar.Collapse id="default-navbar">
                     <Nav className="mr-auto">
-                        <Nav.Link href="#home">{props.home}</Nav.Link>
-                        <Nav.Link href="#login">{props.about}</Nav.Link>
+                        <Nav.Link href="#home">Home</Nav.Link>
                     </Nav>
-                    <Form inline>
-                        <FormControl type="text" isRequired placeholder="Username"
-                            className="mr-sm-2" />
-                        <Button variant="outline-success">Login</Button>
-                    </Form>
                 </Navbar.Collapse>
             </Navbar>
         </>
