b264da8d0f7a2ae0a54c7a11b66f50b3df807295
Luuk Volkering <luuk.volkering@gmail.com>
2021-03-07 20:39:18 +0100

    Made the beginning of a login form

--- src/components/login.js
+++ src/components/login.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Form, Button } from 'react-bootstrap';
+import './login.css';
 
 const Login = () => (
-    <div>
-        <p>
-            You are logged in!
-        </p>
+    <div class="row">
+        <Form class="inputForm">
+            <div class="form-group" flex-direction="column" align="center">
+                <input type="email" class="form-control" id="InputEmail" placeholder="Email address"></input>
+            </div>
+            <div class="form-group">
+                <input type="password" class="form-control" id="InputPassword" placeholder="Password"></input>
+            </div>
+            <Button type="submit" class="btn btn-primary">Login</Button>
+        </Form>
     </div>
 )
 
