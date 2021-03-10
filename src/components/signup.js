9a12215ce0d2c72869b257e4ecf87c4a00bdd1f2
Luuk Volkering <luuk.volkering@gmail.com>
2021-03-10 22:41:44 +0100

    Possible added a redirect function

--- src/components/signup.js
+++ src/components/signup.js
@@ -1,6 +1,7 @@
 import { Auth } from "aws-amplify"
 import React from "react";
 import { Form, Button } from "react-bootstrap"
+import { LinkContainer } from "react-router-bootstrap";
 import './login.scss';
 
 let inputEmailValue;
@@ -13,34 +14,36 @@ function handlePassChange(e) {
     inputPasswordValue = e.target.value;
 }
 
-async function signUp(e) {
-    e.preventDefault();
-    try {
-        const { user } = await Auth.signUp(
-            inputEmailValue,
-            inputPasswordValue,
-        );
-        console.log(user);
-        alert(user);
-        alert("You have to confirm on hourkeeper.net/confirm");
-    } catch (error) {
-        console.log(error);
-        alert(error);
+const signup = props => {
+    async function signUp(e) {
+        e.preventDefault();
+        try {
+            const { user } = await Auth.signUp(
+                inputEmailValue,
+                inputPasswordValue,
+            );
+            console.log(user);
+            props.history.push("/confirm");
+        } catch (error) {
+            console.log(error);
+        }
     }
+    return(
+        <div className="row">
+            <Form className="inputForm border border-primary">
+                <div className="form-group" flex-direction="column" align="center">
+                    <input type="email" className="form-control" name="InputEmail" id="InputEmail" onChange={handleEmailChange} placeholder="Email address"></input>
+                </div>
+                <div className="form-group">
+                    <input type="password" className="form-control" name="InputPassword" id="InputPassword" onChange={handlePassChange} placeholder="Password"></input>
+                </div>
+                <Button type="submit" className="btn btn-primary" onClick={signUp}>Sign up</Button>
+                <LinkContainer to="/confirm">
+                    <Button type="submit" className="btn btn-secondary">Confirm page</Button>
+                </LinkContainer>
+            </Form>
+        </div>
+    )
 }
-
-const signup = () => (
-    <div className="row">
-        <Form className="inputForm border border-primary">
-            <div className="form-group" flex-direction="column" align="center">
-                <input type="email" className="form-control" name="InputEmail" id="InputEmail" onChange={handleEmailChange} placeholder="Email address"></input>
-            </div>
-            <div className="form-group">
-                <input type="password" className="form-control" name="InputPassword" id="InputPassword" onChange={handlePassChange} placeholder="Password"></input>
-            </div>
-            <Button type="submit" className="btn btn-primary" onClick={signUp}>Sign up</Button>
-        </Form>
-    </div>
-)
 
 export default signup;
